1257367346414a33f7d9669b4f7df8b8e8f78126
Addison Dalton <zarch14@gmail.com>
2019-01-13 14:36:08 -0500

    learning pipes.

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,14 +4,21 @@ import { CoursesService } from './courses.service';
 @Component({ 
     selector: 'courses',
     template: `
-        <input [(ngModel)]="email" (keyup.enter)="onKeyUp()" />
+        {{ course.title | uppercase }} <br/>
+        {{ course.students | number}} <br/>
+        {{ course.rating | number:'1.1-1' }} <br/>
+        {{ course.price | currency:'AUD' :'symbol' :'3.2-2' }} <br/>
+        {{ course.releaseDate |date:'shortDate' }} <br/>
+
     `
 })
 
 export class CoursesComponent {
-    email = "me@example.com";
-
-    onKeyUp() {
-        console.log(this.email);
+    course = {
+        title: "The complete angular course",
+        rating: 4.975,
+        students: 30212,
+        price: 190.00,
+        releaseDate: new Date(2016, 3, 1)
     }
 }
