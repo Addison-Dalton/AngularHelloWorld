745bff7d352d62e77d7665fcbb02be560294d870
Addison Dalton <zarch14@gmail.com>
2019-01-13 14:57:44 -0500

    Creating custom pipe.

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,21 +4,13 @@ import { CoursesService } from './courses.service';
 @Component({ 
     selector: 'courses',
     template: `
-        {{ course.title | uppercase }} <br/>
-        {{ course.students | number}} <br/>
-        {{ course.rating | number:'1.1-1' }} <br/>
-        {{ course.price | currency:'AUD' :'symbol' :'3.2-2' }} <br/>
-        {{ course.releaseDate |date:'shortDate' }} <br/>
+        {{ text | summary:10 }}
 
     `
 })
 
 export class CoursesComponent {
-    course = {
-        title: "The complete angular course",
-        rating: 4.975,
-        students: 30212,
-        price: 190.00,
-        releaseDate: new Date(2016, 3, 1)
-    }
+    text = `
+        long dummy text long dummy text long dummy text long dummy text long dummy text long dummy text long dummy text long dummy text long dummy text long dummy text long dummy text long dummy text long dummy text long dummy text long dummy text long dummy text 
+    `
 }
---
+++ src/app/summary.pipe.ts
@@ -0,0 +1,15 @@
+import { Pipe, PipeTransfrom, PipeTransform } from '@angular/core';
+
+@Pipe({
+    name: 'summary'
+})
+export class SummaryPipe implements PipeTransform {
+    transform(value: string, limit?: number) {
+        if (!value) {
+            return null;
+        }
+
+        let actualLimit = (limit) ? limit: 50;
+        return value.substr(0, actualLimit) + '...';
+    }
+}
